8b6229a7c33d1ac1f1bab4ea6e8361e26cb82c14
Timur Yigit <timur.yigit@ogulo.de>
2020-12-02 11:03:10 +0100

    Tutorial 1.4 Add Services (completed)

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 
 import { HeroService } from '../hero.service';
+import { MessageService} from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,16 +14,8 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) {}
-
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
-  getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
-  }
+  constructor(private heroService: HeroService,
+              private messageService: MessageService) {}
 
   ngOnInit() {
     this.getHeroes();
@@ -30,5 +23,12 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-  };
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
 }
