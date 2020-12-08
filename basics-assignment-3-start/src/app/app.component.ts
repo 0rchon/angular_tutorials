c7095ef2295df1bdc6385b11bf16399dbc8e8e33
Timur Yigit <timur.yigit@ogulo.de>
2020-12-08 11:55:38 +0100

    Assignment 3 (completed)

---
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  display = false;
+  clicks = [];
+
+  onClickButton() {
+    this.display === false ? this.display = true : this.display = false ;
+    this.clicks.push(this.clicks.length + 1);
+  }
+
+  getColor(click) {
+    return click >= 5 ? 'blue' : 'transparent';
+  }
+}
