643b76b4b4918a32208eea143dd7b34da9283be2
Timur Yigit <timur.yigit@ogulo.de>
2021-01-13 18:11:36 +0100

    new interceptor logic

--- course-project/src/app/header/header.component.ts
+++ course-project/src/app/header/header.component.ts
@@ -1,15 +1,30 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
-  constructor(private dataStorageService: DataStorageService) { }
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
+  private userSubscription: Subscription;
+
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
+    this.userSubscription = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+    });
+  }
+
+  ngOnDestroy() {
+    this.userSubscription.unsubscribe();
   }
 
   onSaveData() {
@@ -19,5 +34,4 @@ export class HeaderComponent implements OnInit {
   onFetchData() {
     this.dataStorageService.fetchRecipes().subscribe();
   }
-
 }
--- course-project/src/app/shared/data-storage.service.ts
+++ course-project/src/app/shared/data-storage.service.ts
@@ -1,40 +1,50 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Recipe } from "../recipes/recipe.model";
-import { RecipeService } from "../recipes/recipe.service";
-import { map, tap } from "rxjs/operators";
-import { pipe } from "rxjs";
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
+import { exhaustMap, map, take, tap } from 'rxjs/operators';
+import { pipe } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
 
-    this.http.put(
-      'https://ng-course-recipe-book-a2cbf-default-rtdb.firebaseio.com/recipes.json',
-      recipes
-    ).subscribe(
-      response => {
+    this.http
+      .put(
+        'https://ng-course-recipe-book-a2cbf-default-rtdb.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe((response) => {
         console.log(response);
-      }
-    );
+      });
   }
 
   fetchRecipes() {
-    return this.http
-    .get<Recipe[]>(
-      'https://ng-course-recipe-book-a2cbf-default-rtdb.firebaseio.com/recipes.json',
-    )
-    .pipe(
+    return this.authService.user.pipe(
+      take(1),
+      exhaustMap((user) => {
+        return this.http.get<Recipe[]>(
+          'https://ng-course-recipe-book-a2cbf-default-rtdb.firebaseio.com/recipes.json',
+          {
+            params: new HttpParams().set('auth', user.token)
+          }
+        );
+      }),
       map(recipes => {
-        return recipes.map(recipe => {
+        return recipes.map((recipe) => {
           return {
             ...recipe,
-            ingredients: recipe.ingredients ? recipe.ingredients : []
-          }
-        })
+            ingredients: recipe.ingredients ? recipe.ingredients : [],
+          };
+        });
       }),
       tap(recipes => {
         this.recipeService.setRecipes(recipes);
