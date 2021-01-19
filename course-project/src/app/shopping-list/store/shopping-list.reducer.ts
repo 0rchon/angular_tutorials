4c4bcfc9c73c421f7dc558c231bf891b14ead8d0
Timur Yigit <timur.yigit@ogulo.de>
2021-01-20 00:39:43 +0100

    new structure of initialState

--- course-project/src/app/shopping-list/store/shopping-list.reducer.ts
+++ course-project/src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,14 +1,24 @@
 import { Ingredient } from '../../shared/ingredient.model';
 import * as ShoppingListActions from './shopping-list.actions';
 
-const initialState = {
+export interface State {
+  ingredients: Ingredient[];
+  editedIngredient: Ingredient;
+  editedIngredientIndex: number;
+}
+
+export interface AppState {
+  shoppingList: State;
+}
+
+const initialState: State = {
   ingredients: [new Ingredient('tomatoes', 3), new Ingredient('lamb meat', 1)],
-  // editedIngredient: null,
-  // editedIngredientIndex: -1,
+  editedIngredient: null,
+  editedIngredientIndex: -1,
 };
 
 export function shoppingListReducer(
-  state = initialState,
+  state: State = initialState,
   action: ShoppingListActions.ShoppingListActions
 ) {
   switch (action.type) {
