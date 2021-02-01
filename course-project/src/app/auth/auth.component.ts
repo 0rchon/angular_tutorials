adeeeda359bcdd01ca8b53f2e679ba56915defa6
Timur Yigit <timur.yigit@ogulo.de>
2021-02-01 16:29:44 +0100

    auth effects added

--- course-project/src/app/auth/auth.component.ts
+++ course-project/src/app/auth/auth.component.ts
@@ -1,16 +1,24 @@
-import { Component, ComponentFactoryResolver, OnDestroy, ViewChild } from '@angular/core';
+import {
+  Component,
+  ComponentFactoryResolver,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
 import { AlertComponent } from '../shared/alert/alert.component';
 import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
 import { AuthService, AuthResponseData } from './auth.service';
+import * as fromApp from '../store/app.reducer';
+import * as AuthActions from './store/auth.actions';
 
 @Component({
   selector: 'app-auth',
   templateUrl: 'auth.component.html',
 })
-export class AuthComponent implements OnDestroy{
+export class AuthComponent implements OnDestroy {
   isLoginMode = true;
   isLoading = false;
   error: string = null;
@@ -20,9 +28,9 @@ export class AuthComponent implements OnDestroy{
   constructor(
     private authService: AuthService,
     private router: Router,
-    private componentFactoryResolver: ComponentFactoryResolver
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private store: Store<fromApp.AppState>
   ) {}
-
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -41,7 +49,10 @@ export class AuthComponent implements OnDestroy{
     let authObs: Observable<AuthResponseData>;
 
     if (this.isLoginMode) {
-      authObs = this.authService.login(email, password);
+      // authObs = this.authService.login(email, password);
+      this.store.dispatch(
+        new AuthActions.LoginStart({ email: email, password: password })
+      );
     } else {
       authObs = this.authService.signUp(email, password);
     }
@@ -58,35 +69,35 @@ export class AuthComponent implements OnDestroy{
         this.showErrorAlert(errorMessage);
         this.isLoading = false;
       }
-      );
+    );
 
-      form.reset();
+    form.reset();
+  }
+
+  onHandleError() {
+    this.error = null;
+  }
+
+  ngOnDestroy() {
+    if (this.closeSub) {
+      this.closeSub.unsubscribe();
     }
+  }
 
-    onHandleError() {
-      this.error = null;
-    }
+  private showErrorAlert(message: string) {
+    const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(
+      AlertComponent
+    );
 
-    ngOnDestroy() {
-      if (this.closeSub) {
-        this.closeSub.unsubscribe();
-      }
-    }
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+    hostViewContainerRef.clear();
 
-    private showErrorAlert(message: string) {
-      const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(
-        AlertComponent
-        );
+    const componentRef = hostViewContainerRef.createComponent(alertCmpFactory);
 
-        const hostViewContainerRef = this.alertHost.viewContainerRef;
-        hostViewContainerRef.clear();
-
-        const componentRef = hostViewContainerRef.createComponent(alertCmpFactory);
-
-        componentRef.instance.message = message;
-        this.closeSub = componentRef.instance.close.subscribe(() => {
-          this.closeSub.unsubscribe();
-          hostViewContainerRef.clear();
-        });
+    componentRef.instance.message = message;
+    this.closeSub = componentRef.instance.close.subscribe(() => {
+      this.closeSub.unsubscribe();
+      hostViewContainerRef.clear();
+    });
   }
 }
